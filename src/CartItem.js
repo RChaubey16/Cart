4c7d46aa0b46ac7564ffd3b264deae8817dea718
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-26 10:56:19 +0530

    Fourth Commit: Added action-icons and also created state for the component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor () {
+        super();
+        // Adding state to the component to store the local data of the component
+        this.state = {
+            price: 1999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render() {
+        const { price, title, qty } = this.state                                          // Object Destructuring
         return (
             <div className="cart-item">
                 
@@ -9,11 +20,14 @@ class CartItem extends React.Component{
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ { fontSize: 25 } }>Phone</div>                            {/* Created the styling using JS object right away */}
-                    <div style={ { color: "grey" } }>Rs 999</div>
-                    <div style={ { color: "grey" } }>Qty: 1</div>
+                    <div style={ { fontSize: 25 } }> {title} </div>                            {/* Created the styling using JS object right away */}
+                    <div style={ { color: "grey" } }>Rs {price} </div>
+                    <div style={ { color: "grey" } }>Qty: {qty} </div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png" />
                     </div>
                 </div>
             </div>        
@@ -24,8 +38,9 @@ class CartItem extends React.Component{
 // Styling using objects in JSX
 const styles = {
     image: {
-        height: 110,
-        width: 110,
+        // Do not need to write px as JSX adds it automatically
+        height: 140,
+        width: 140,
         borderRadius: 4,
         background: '#ccc'
     }
