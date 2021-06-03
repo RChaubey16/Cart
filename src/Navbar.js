a55fae191e07b1d386ee346f80b2163e0aadc570
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-06-03 12:52:14 +0530

    Add the Navbar Component

---
+++ src/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div style={styles.nav}>
+      <div style={styles.cartIconContainer}>
+        {/* image of cart */}
+        <img
+          style={styles.cartIcon}
+          src="https://image.flaticon.com/icons/png/512/1170/1170678.png"
+          alt="Cart-Icon"
+        />
+        {/* the count of products in cart */}
+        <span style={styles.cartCount}>3</span>
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  cartIcon: {
+    height: 32,
+    marginRight: 40,
+    cursor: "pointer",
+  },
+  nav: {
+    height: 70,
+    background: "#4267b2",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  cartIconContainer: {
+    position: "relative",
+  },
+  cartCount: {
+    background: "yellow",
+    borderRadius: "50%",
+    padding: "4px 8px",
+    position: "absolute",
+    right: 20,
+    top: -9,
+  },
+};
+
+export default Navbar;
