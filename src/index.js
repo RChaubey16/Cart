fafbfe609e36a73ce264c32a53b8c1184ce5eced
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-06-14 09:51:45 +0530

    Added firebase to our project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCAdEEyBT0jzWY4H0mkjfc74p3sLceSR4",
+  authDomain: "cart-9affe.firebaseapp.com",
+  projectId: "cart-9affe",
+  storageBucket: "cart-9affe.appspot.com",
+  messagingSenderId: "824499035985",
+  appId: "1:824499035985:web:bdfb0fac21ab9b402cbd70",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
